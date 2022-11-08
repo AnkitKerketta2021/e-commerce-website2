00085c2877b79c01bb6345a079082f8e10d7dc1f
Ankit <ankitkerketta999@gmail.com>
2022-11-08 12:02:46 +0530

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h2 style={{ textAlign: "center" }}>E-Commerce Website</h2>
+      <h2 style={{ textAlign: "center", fontFamily:"sans-serif" }}>E-Commerce Website</h2>
     </div>
   );
 }
